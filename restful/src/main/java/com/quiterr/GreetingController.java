fae20657c154ddb0a57e2fb203346b94de3d9ee9
黄宸 <huangchen@iot.chinamobile.com>
2017-05-09 17:13:54 +0800

    Initial commit

---
+++ restful/src/main/java/com/quiterr/GreetingController.java
@@ -0,0 +1,27 @@
+package com.quiterr;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Huangchen on 2017/5/8.
+ */
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    /**
+    * The example does not specify GET vs. PUT, POST, and so forth,
+    * because @RequestMapping maps all HTTP operations by default.
+    * Use @RequestMapping(method=GET) to narrow this mapping.
+     */
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
