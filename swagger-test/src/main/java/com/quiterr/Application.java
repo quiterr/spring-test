194bce2fc51d151e7c112df4ae0f288318e2fa9d
quiterr <670758823@qq.com>
2017-05-14 22:13:21 +0800

    swagger

---
+++ swagger-test/src/main/java/com/quiterr/Application.java
@@ -0,0 +1,15 @@
+package com.quiterr;
+
+/**
+ * Created by Huangchen on 2017/5/9.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
