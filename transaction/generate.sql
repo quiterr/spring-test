4efcfa683418bf78ce7496e9e4177046b4957825
黄宸 <huangchen@iot.chinamobile.com>
2017-05-18 10:34:36 +0800

    添加创建数据表的sql文件

---
+++ transaction/generate.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user` (
+	`id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+	`age` INT(11) NOT NULL,
+	`name` VARCHAR(255) NOT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
