510eed13f8f1577bf22d423c82da0743a097794d
黄宸 <huangchen@iot.chinamobile.com>
2017-05-18 15:56:26 +0800

    SpringBoot @Async异步调用

---
+++ async/src/main/java/com/quiterr/Application.java
@@ -0,0 +1,17 @@
+package com.quiterr;
+
+/**
+ * Created by Huangchen on 2017/5/9.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
